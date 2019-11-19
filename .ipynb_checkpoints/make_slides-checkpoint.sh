443996669a678da5770d7d45ec72ac883eeb1803
Krzysztof Basiński <k.basinski@gumed.edu.pl>
2019-11-19 11:50:32 +0100

    update w02

---
+++ .ipynb_checkpoints/make_slides-checkpoint.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+pandoc -t revealjs -s w01.md -o w01.html -i
+pandoc -t revealjs -s w02.md -o w02.html -i
+pandoc -t revealjs -s w03.md -o w03.html -i
+pandoc -t revealjs -s w04.md -o w04.html -i
+pandoc -t revealjs -s w05.md -o w05.html -i
+pandoc -t revealjs -s w06.md -o w06.html -i
+pandoc -t revealjs -s w07.md -o w07.html -i

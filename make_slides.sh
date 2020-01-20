87e7034c5300d54ead6d427672ef51ab40d38f39
Krzysztof Basiński <k.basinski@gumed.edu.pl>
2020-01-20 12:08:18 +0100

    drobnostki

--- make_slides.sh
+++ make_slides.sh
@@ -2,7 +2,7 @@
 
 pandoc -t revealjs -s w01.md -o w01.html -i
 pandoc -t revealjs -s w02.md -o w02.html -i
-pandoc -t revealjs -s w03.md -o w03.html -i
+# pandoc -t revealjs -s w03.md -o w03.html -i
 pandoc -t revealjs -s w04.md -o w04.html -i
 pandoc -t revealjs -s w05.md -o w05.html -i
 pandoc -t revealjs -s w06.md -o w06.html -i
